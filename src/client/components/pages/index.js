5b9ca0d19283a203a62f5fb37cc2269fd0a29213
DemonSheriff <anantshukla404@gmail.com>
2024-06-21 16:15:00 +0530

    changed the logo and name of X to it's older versions

--- src/client/components/pages/index.js
+++ src/client/components/pages/index.js
@@ -21,15 +21,13 @@
 
 import * as bootstrap from 'react-bootstrap';
 
-import { faCircle, faCommentDots, faComments, faEnvelope, faListUl, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
+import {faCircle, faCommentDots, faComments, faEnvelope, faListUl, faSearch, faUser} from '@fortawesome/free-solid-svg-icons';
 
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import PropTypes from 'prop-types';
 import React from 'react';
 import RevisionsTable from './parts/revisions-table';
-
-
+import {faTwitter} from '@fortawesome/free-brands-svg-icons';
 
 
 const {Alert, Button, Col, Container, Row} = bootstrap;
